21de0cf57d5bf7eb3bcb36f515023b4bbf68edb8
Gopinath <48796140+GopinathAP55@users.noreply.github.com>
2020-05-01 20:33:45 +0530

    speed test

--- controller/userDetails.controller.js
+++ controller/userDetails.controller.js
@@ -4,7 +4,7 @@ const UserDetails = mongoose.model('userDetails');
 
 const _ = require('lodash');
 //Implemenation of the register funstion and exporting it.
-module.exports.register = (req,res,next)=>{
+module.exports.register = (req, res, next) => {
     console.log('insert user')
     var newUser = new UserDetails();
     newUser.username = req.body.username;
@@ -13,81 +13,105 @@ module.exports.register = (req,res,next)=>{
     newUser.password = req.body.password;
     newUser.geetValue = req.body.geetValue;
     console.log(req.body);
-    newUser.save((err , doc)=>{
+    newUser.save((err, doc) => {
         console.log('inside insert fn');
-        if(!err){
-            return res.status(200).json({"message":'Make way for'+ req.body.username})  ;     
-        }else{
+        if (!err) {
+            return res.status(200).json({ "message": 'Make way for' + req.body.username });
+        } else {
             console.log(err);
-            return res.status(400).json({"message":'MONGO thinks the EMAIL ID already exists'});
+            return res.status(400).json({ "message": 'MONGO thinks the EMAIL ID already exists' });
         }
-    }); 
+    });
 }
 //Implemenation of the login function and exporting it.
 
-module.exports.login = (req,res, next)=>{
+module.exports.login = (req, res, next) => {
     console.log('inside login fn');
-     UserDetails.findOne({_id:req._id}, function(err, UserDetails) {
+    UserDetails.findOne({ _id: req._id }, function (err, UserDetails) {
         if (!UserDetails) {
-            res.status(404).json({status:false,message:'sorry no user..'});
-        }else{
-            res.status(200).json({status:false,UserDetails : _.pick(UserDetails,['username','email'])});
+            res.status(404).json({ status: false, message: 'sorry no user..' });
+        } else {
+            res.status(200).json({ status: false, UserDetails: _.pick(UserDetails, ['username', 'email']) });
         }
-      });  
+    });
 }
 
-module.exports.authenticate = (req,res,next)=>{
+module.exports.authenticate = (req, res, next) => {
     console.log('authenticate error');
-    passport.authenticate('local',(err,userDetails,info)=>{
+    passport.authenticate('local', (err, userDetails, info) => {
         console.log('authenticate');
-        if(err){
+        if (err) {
             return res.status(400).json(err);
-        }else if (userDetails){
-            return res.status(200).json({"token": userDetails.generateJwt()});
-        }else{
+        } else if (userDetails) {
+            return res.status(200).json({ "token": userDetails.generateJwt() });
+        } else {
             return res.status(404).json(info);
         }
-    }) (req,res);
+    })(req, res);
 }
 
 
-module.exports.allGeet = (req,res,next)=>{
+module.exports.allGeet = (req, res, next) => {
     console.log('inside all geet');
-    UserDetails.find({}).toArray(function(err, result){
-        if(err){
+    UserDetails.find({}).toArray(function (err, result) {
+        if (err) {
             res.status(400).json(err);
             console.log('err');
-        }else{
-            res.status(200).json({status:false,UserDetails : _.pick(UserDetails,['username','email'])});
+        } else {
+            res.status(200).json({ status: false, UserDetails: _.pick(UserDetails, ['username', 'email']) });
             console.log('success');
         }
     });
 }
 
-module.exports.geet = (req,res,next)=>{
+module.exports.geet = (req, res, next) => {
     console.log(req.headers.authorization);
     var token = req.headers.authorization
-    if(token){ 
+    if (token) {
         var userPayLoad = (token.split('.')[1]);
 
-      
-        var decodedValue =   Buffer.from(userPayLoad, 'base64').toString() ; 
+
+        var decodedValue = Buffer.from(userPayLoad, 'base64').toString();
         console.log(decodedValue);
 
-        
 
-        UserDetails.findByIdAndUpdate({_id:'5d899e097329fb4e8856a2f3'},{$set: {'geetValue':req.body.geetValue}},(err,user)=>{
-            if(err){
+
+        UserDetails.findByIdAndUpdate({ _id: '5d899e097329fb4e8856a2f3' }, { $set: { 'geetValue': req.body.geetValue } }, (err, user) => {
+            if (err) {
                 console.log(err)
-            }else{
+            } else {
                 console.log(user)
             }
         })
-       
-     }
 
-
-
-   //Appiness Project
     }
-   
+
+    module.exports.speed = (req, res) => {
+
+        const FastSpeedtest = require('fast-speedtest-api');
+        console.log('')
+        let speedtest = new FastSpeedtest({
+            token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", // required
+            verbose: false, // default: false
+            timeout: 10000, // default: 5000
+            https: true, // default: true
+            urlCount: 5, // default: 5
+            bufferSize: 8, // default: 8
+            unit: FastSpeedtest.UNITS.Mbps// default: Bps
+        });
+
+        speedtest.getSpeed().then(s => {
+            console.log(`Speed: ${s} Mbps`);
+        }).catch(e => {
+            console.error(e.message);
+        });
+
+
+        console.log('this is a GET');
+        res.writeHead(200);res.end();
+
+
+    }
+
+    //Appiness Project
+}
--- routes/index.route.js
+++ routes/index.route.js
@@ -16,9 +16,31 @@ router.get('/allGeet',jwtHelper.verifyJwtToken,userDetailscontroller.allGeet);
 //Exporting the router details using module.exports. This can be used in app.js
 router.post('/authenticate',userDetailscontroller.authenticate);
 
+//to get the speed
+router.get('/speed',function(req,res){
 
-//Appiness project
+    const FastSpeedtest = require('fast-speedtest-api');
+    console.log('sppe')
+    let speedtest = new FastSpeedtest({
+        token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", // required
+        verbose: false, // default: false
+        timeout: 10000, // default: 5000
+        https: true, // default: true
+        urlCount: 5, // default: 5
+        bufferSize: 8, // default: 8
+        unit: FastSpeedtest.UNITS.Mbps// default: Bps
+    });
 
+    speedtest.getSpeed().then(s => {
+        console.log(`Speed: ${s} Mbps`);
+        res.send(`Speed: ${s} Mbps`)
+        res.sendStatus(200)
+    }).catch(e => {
+        console.error(e.message);
+    });
+
+    console.log('final')
+})
 
 
 
